d1cd81a1f6e6b8404dcb4dd91f79e48d0dc809e4
VinvanGo <vinvin021205@gmail.com>
2026-03-29 13:52:33 +0800

    Form1.cs

--- TestWins/Form1.cs
+++ TestWins/Form1.cs
@@ -1,5 +1,7 @@
 using System.Drawing.Text;
+using System.Xml.Linq;
 using TestWins.Controller;
+using TestWins.Model;
 
 namespace TestWins;
 
@@ -8,6 +10,7 @@ public partial class Form1 : Form
     //business
 
     private readonly StudentController controller = new StudentController();
+
     public Form1()
     {
         InitializeComponent();
@@ -21,23 +24,53 @@ public partial class Form1 : Form
 
     private void btnAdd_Click(object sender, EventArgs e)
     {
+        var student = new Student
+        {
+            Id = int.Parse(txtId.Text),
+            Name = txtName.Text,
+            Course = txtCourse.Text,
+            Year = int.Parse(txtYear.Text)
+        };
 
+        controller.create(student);
+        loadData();
     }
 
     private void btnUpdate_Click(object sender, EventArgs e)
     {
+        var student = new Student
+        {
+            Id = int.Parse(txtId.Text),
+            Name = txtName.Text,
+            Course = txtCourse.Text,
+            Year = int.Parse(txtYear.Text)
+        };
 
+        controller.update(student);
+        loadData();
     }
 
     private void btnDelete_Click(object sender, EventArgs e)
     {
+        int id = int.Parse(txtId.Text);
 
 
 
+        controller.delete(id);
+        loadData();
     }
 
     private void dataGridView1_CellClick(object sender, EventArgs e)
+    private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
     {
+        if (e.RowIndex >= 0)
+        {
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
 
+            txtId.Text = row.Cells["Id"].Value.ToString();
+            txtName.Text = row.Cells["Name"].Value.ToString();
+            txtCourse.Text = row.Cells["Course"].Value.ToString();
+            txtYear.Text = row.Cells["Year"].Value.ToString();
+        }
     }
 }
